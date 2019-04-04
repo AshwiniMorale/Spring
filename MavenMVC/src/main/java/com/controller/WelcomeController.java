7ae8721633fb24217f2dbc43567c865d645dabc6
cyber <cyber@cyber-PC>
2019-04-04 10:16:40 +0530

    new

--- MavenMVC/src/main/java/com/controller/WelcomeController.java
+++ MavenMVC/src/main/java/com/controller/WelcomeController.java
@@ -1,18 +1,22 @@
 package com.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
 public class WelcomeController 
 {
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String welcome(Model model)
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public ModelAndView welcome(HttpServletRequest req, HttpServletResponse res)
 	{
-		model.addAttribute("message", "hi spring mvc framework");
-		return "welcome";
+		System.out.println("controller");
+		//model.addAttribute("message", "hi spring mvc framework");
+		
+		return new ModelAndView("index","message","Welcome to Cyber");
 	}
 
 }
